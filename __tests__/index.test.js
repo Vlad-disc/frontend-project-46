a30ebf8d58b637ec12825a36891c03c5a8415498
Усков Владислав <uskov_vladislav@icloud.com>
2024-08-24 15:33:21 +0300

    add fix

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,4 @@
-import { describe, expect, test } from '@jest/globals';
+import { describe, expect } from '@jest/globals';
 import readFile from '../src/reader.js';
 import genDiff from '../src/index.js';
 
