2c1a3313e2b143d5b17e841f0d1867836d3dd9ee
Усков Владислав <uskov_vladislav@icloud.com>
2024-08-29 16:21:15 +0300

    bugs

--- Makefile
+++ Makefile
@@ -14,7 +14,7 @@ gendiff -h:
 	node bin/gendiff.js
 
 test:
-	NODE_OPTIONS=--experimental-vm-modules npx jest
+	npx jest --runInBand --bail
 
 test-coverage:   	
 	npm test -- --coverage --coverageProvider=v8
--- src/reader.js
+++ src/reader.js
@@ -5,7 +5,7 @@ import path from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const getPath = (filename) => path.resolve(__dirname, '..', '__fixtures__', filename);
+const getPath = (filename) => path.resolve(__dirname, '..', filename);
 const readFile = (filename) => fs.readFileSync(getPath(filename), 'utf-8');
 
 export default readFile;
